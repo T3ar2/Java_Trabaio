7dd4577548c962057f23b4c3076f6ca8e2df0daf
Fernando <luiz.santoz2510@gmail.com>
2025-05-09 20:15:11 -0300

    LOVE LOVE FOREVER, MY BLOOD BOILS FOR LOVE LIVE FOREVER, I LOVE LOVE LIVE WITH ALL MY LIFE, I WOULD DIE FOR LOVE LIVE

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main{
     public static void main(String[] args) {
 /*TESTANDO-CHAN */
         /*RUBY CHAN!!! NANI GA SUKI?????*/
-        /*HAIIIIIII, CHOKO MINTO, NARE MA NA ANATA!*/
+        /*HAIIIIIII, CHOKO MINTO, NARE MA NA ANATA!!*/
     }
 }
